635351e77fe45af3e1906f42abc42488090e099c
apalvai <anoopreddy06@gmail.com>
2017-09-02 18:23:50 -0700

    Improved the drive by increasing the d coefficient.

--- src/main.cpp
+++ src/main.cpp
@@ -35,10 +35,10 @@ int main()
     PID pid_steer, pid_throttle;
     
     // sterring
-    pid_steer.Init(0.2, 0.001, 5.0);
+    pid_steer.Init(0.2, 0.001, 10.0);
     
     // throttle
-    pid_throttle.Init(0.2, 0.001, 5.0);
+    pid_throttle.Init(0.2, 0.001, 10.0);
     
     h.onMessage([&pid_steer, &pid_throttle](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
         // "42" at the start of the message means there's a websocket message event.
